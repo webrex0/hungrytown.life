64a0a62122ffc594f64fcabb5dba981acba4bd91
webrex0 <137759862+webrex0@users.noreply.github.com>
2023-11-01 13:58:21 +0800

    update test login

--- numberlogin/login.php
+++ numberlogin/login.php
@@ -1,41 +1,32 @@
 <?php
 session_start();
 
-// Replace with your actual database connection code
-$servername = "localhost";
-$username = "user8899";
-$password = "pass8899";
-$dbname = "HQuser";
-
-// Create a database connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+function getUserData($filename) {
+    $users = [];
+    $lines = file($filename, FILE_IGNORE_NEW_LINES);
+    if ($lines) {
+        foreach ($lines as $line) {
+            list($username, $password) = explode(':', $line);
+            $users[trim($username)] = trim($password);
+        }
+    }
+    return $users;
 }
+
+$usersFile = 'users.txt';
+$users = getUserData($usersFile);
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST["username"];
     $password = $_POST["password"];
 
-    // Validate user against the database
-    $sql = "SELECT * FROM users WHERE username = ? AND password = ?";
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("ss", $username, $password);
-    $stmt->execute();
-    $result = $stmt->get_result();
-
-    if ($result->num_rows == 1) {
+    if (isset($users[$username]) && $users[$username] === $password) {
         // Login successful, store user information in the session
         $_SESSION['username'] = $username;
-        header("Location: numero.html");
+        header("Location: numberlogin/numero.html");
         exit;
     } else {
         echo "Login failed. Please check your username and password.";
     }
-
-    $stmt->close();
 }
-
-$conn->close();
 ?>
